99e190cacb7d385474b755e5746f7b1f9aeea342
unknown <neer8669@gmail.com>
2026-03-05 13:38:44 +0530

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://agentic-ai-1-sedl.onrender.com", "https://agentic-ai-backend-i4uy.onrender.com"],
+    allow_origins=["https://agentic-ai-1-sedl.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
